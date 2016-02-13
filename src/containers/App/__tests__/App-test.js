39f636e907aee733c275585f2f47123112f8839d
Sebastian Deutsch <sebastian.deutsch@9elements.com>
2016-02-13 23:58:34 +0100

    Adapted style

--- src/containers/App/__tests__/App-test.js
+++ src/containers/App/__tests__/App-test.js
@@ -1,10 +1,8 @@
-import _ from 'lodash';
 import React from 'react';
 import TestUtils from 'react-addons-test-utils';
 import { App } from 'containers/App';
 
 describe('Login dumb', () => {
-
   it('should render', () => {
     expect(1).toEqual(1);
   });
