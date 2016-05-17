478678608f97c3670ce9a3f87e1e6fae55f08c60
Sebastian Deutsch <sebastian.deutsch@9elements.com>
2016-05-17 23:27:46 +0200

    Step 6: announce reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import app                 from 'reducers/app';
+import books               from 'reducers/books';
 
 const rootReducer = combineReducers({
-  app
+  app,
+  books
 });
 
 export default rootReducer;
